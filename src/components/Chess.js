7a36f6292728d713ca0003c2bdbe06553a8ceacf
TruongHoaiLinh3103 <eagleremlinh@gmail.com>
2025-04-20 15:04:37 +0700

    Chess

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -7,6 +7,14 @@ const Chess = () => {
     return (
         <div className='chess'>
             <div className='c-chess'>
+                <a href={chess} download="Cờ Vây" target='_blank'>
+                    <button className="c-chess_button" href={chess}>
+                        <span className="c-chess_button_lg">
+                            <span className="c-chess_button_sl"></span>
+                            <span className="c-chess_button_text">Download Now</span>
+                        </span>
+                    </button>
+                </a>
                 <MobilePDFReader url={chess}/>
             </div>
         </div>
