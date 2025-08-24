c440dcea357651155e60831a3cfbc91809e2e42f
Trương Hoài Linh <131947376+TruongHoaiLinh3103@users.noreply.github.com>
2025-08-24 20:35:06 +0700

    Update Calligraphy.js

--- src/components/Calligraphy.js
+++ src/components/Calligraphy.js
@@ -23,8 +23,8 @@ const Calligraphy = () => {
                     <h2>{text}</h2>
                 </div> :
                 <div className='Calligraphy-show'>
-                    <h2>A(a) Ă(ă) Â(â) B(b) C(c) D(d) Đ(đ) E(e) Ê(ê) F(f) G(g) H(h) I(i) J(j) K(k) L(l) M(m) N(n) O(o) Ô(ô) Ơ(ơ) P(p) Q(q) R(r) S(s) T(t) U(u) Ư(ư) V(v) W(w) X(x) Y(y) Z(z)</h2>
-                    <h2>Nặng, Sắt, Ngã</h2>
+                    <h2>A(a) Ă(ă) Â(â) B(b) C(c) D(d) Đ(đ) E(e) Ê(ê) G(g) H(h) I(i) K(k) L(l) M(m) N(n) O(o) Ô(ô) Ơ(ơ) P(p) Q(q) R(r) S(s) T(t) U(u) Ư(ư) V(v) X(x) Y(y)</h2>
+                    <h2>Nặng, Sắt, Ngã, Huyền, Hỏi, Ngang, Dọc</h2>
                 </div>
             }
         </div>
